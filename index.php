62094392500ac037e4e1c135582ed4550eb3363a
subrotoice <subroto.iu@gmail.com>
2020-07-20 07:31:57 +0600

    init

---
+++ index.php
@@ -0,0 +1,43 @@
+<?php
+class Fruit {
+  // Properties
+  public $name;
+  public $color;
+
+  // Methods
+ 
+  function get_name() {
+    return $this->name;
+  }
+}
+
+class MyFruit extends Fruit {
+    public $weight;
+    
+    // Methods
+    public function __construct($name, $color, $weight)
+    {
+        $this->name = $name;
+        $this->color = $color;
+        $this->weight = $weight;
+    }
+
+    function showAll () {
+        echo $this->weight.$this->color.$this->name;
+    }
+
+    const MYMESSAGE = "Here is ok";
+
+    public satic function testStatic() {
+        echo " Fine Static.";
+    }
+}
+
+$apple = new MyFruit("Apple ok", "Orange", "34");
+$apple->showAll();
+echo MyFruit::MYMESSAGE;
+?>
+
+<?php 
+echo
+?>
